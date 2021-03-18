26f1d28ebe55768ce011676df96e29c099f0a1e5
JonasDudonis <74532923+JonasDudonis@users.noreply.github.com>
2021-03-18 23:19:42 +0200

    task 1 - DB_READ commit

--- index.php
+++ index.php
@@ -14,16 +14,51 @@
         $username = 'root';
         $password = 'mysql';
         $dbname = 'projects';
+        $table = 'darbuotojai';
+
+        if(isset($_GET['path']) and $_GET['path'] !== $table){
+            if($_GET['path'] == 'darbuotojai' or $_GET['path'] == 'projektai')
+                $table = $_GET['path'];
+        }
 
         $conn = mysqli_connect($servername, $username, $password, $dbname);
         if(!$conn)
-        die("Connection failed: ". mysqli_connect_error());
+        die("Connection failed: ". mysqli_connect_error());        
+
+        $sql = "SELECT " . $table. ".id, " . $table.".name, " . ($table === 'projektai' ? 'darbuotojai' : 'projektai' ) . ".name " . "FROM " . $table . 
+            " LEFT JOIN " . ($table === 'projektai' ? 'darbuotojai' : 'projektai') . 
+            " ON " . ($table === 'projektai' ? 'darbuotojai.proj_id = projektai.id' : 'darbuotojai.proj_id = projektai.id');
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $stmt->bind_result($id, $mainProjectName, $relatedEmployeeName);
+?>
+    <header>
+        <div>
+            <a href="?path=projektai" class="" style="">Projekto valdymas</a>
+            <ul id="nav-mobile" class="left">
+            <li><a href="?path=projektai">Projektai</a></li>
+            <li><a href="?path=darbuotojai">Darbuotojai</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <?php
+        echo '<table><th>Id</th><th>Name</th><th>' . ($table === 'projektai' ? 'Darbuotojai' : 'Projektai') . '</th>';
+        while ($stmt->fetch()){
+        echo "<tr><td>" . $id . "</td><td>" . $mainProjectName . "</td><td>" . $relatedEmployeeName . "</td></tr>";
+    }
+        echo '</table>';
+    ?>
+
+    <?php
+
+    // CREATE NEW EMPLOYEE LOGIC
 
         if(isset($_POST['create_employee'])){
-            $stmt = $conn->prepare('INSERT INTO darbuotojai (fname, lname) VALUES (?, ?)');
-            $stmt->bind_param('ss', $fname, $lname);
-            $fname = $_POST['fname'];
-            $lname = $_POST['lname'];
+            $stmt = $conn->prepare('INSERT INTO darbuotojai (name) VALUES (?)');
+            $stmt->bind_param('s', $name);
+            $name = $_POST['name'];
             
             $stmt->execute();
             $stmt->close();
@@ -36,7 +71,7 @@
 
     if ($result->num_rows > 0) {
         while($row = $result->fetch_assoc()) {
-          echo "id: " . $row["id"]. " - Name: " . $row["fname"]. " " . $row["lname"]. " " . " ";
+          echo "id: " . $row["id"]. " - Name: " . $row["name"]. " ";
           print("<button>DELETE</button><br>");
         }
     } else {
@@ -45,12 +80,11 @@
     
     $conn->close();
     ?>
+
     <br>
     <form action="" method="POST">
         <label for="fname">Name:</label><br>
-        <input type="text" id="fname" name="fname" value=""><br>
-        <label for="lname">Surname:</label><br>
-        <input type="text" id="lname" name="lname" value=""><br>
+        <input type="text" id="name" name="name" value=""><br>
         <input type="submit" name="create_employee" value="Submit">
     </form>
 </body>
