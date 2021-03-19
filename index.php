45703be5a79341ad45af6e089ab8d3923683ee84
JonasDudonis <74532923+JonasDudonis@users.noreply.github.com>
2021-03-19 20:33:58 +0200

    group_concat commit

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link type="text/css" rel="stylesheet" href="materialize.css"  media="screen,projection"/>
-    <link type="text/css" rel="stylesheet" href="style.css"  media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="materialize.css" media="screen,projection"/>
+    <link type="text/css" rel="stylesheet" href="style.css" media="screen,projection"/>
     <title>CRUD-Project-Manager-PHP</title>
 </head>
 <body>
@@ -25,16 +25,31 @@
         if(!$conn)
         die("Connection failed: ". mysqli_connect_error());        
 
-        $sql = "SELECT " . $table. ".id, " . $table.".name, " . ($table === 'projektai' ? 'darbuotojai' : 'projektai' ) . ".name " . "FROM " . $table . 
-            " LEFT JOIN " . ($table === 'projektai' ? 'darbuotojai' : 'projektai') . 
-            " ON " . ($table === 'projektai' ? 'darbuotojai.proj_id = projektai.id' : 'darbuotojai.proj_id = projektai.id');
+        // TASK 1 - READ TABLES
+
+        // $sql = "SELECT " . $table. ".id, " . $table.".name, " . ($table === 'projektai' ? 'darbuotojai' : 'projektai' ) . ".name " . "FROM " . $table . 
+        //     " LEFT JOIN " . ($table === 'projektai' ? 'darbuotojai' : 'projektai') . 
+        //     " ON " . ($table === 'projektai' ? 'darbuotojai.proj_id = projektai.id' : 'darbuotojai.proj_id = projektai.id');
+        
+        // TASK 2 - AGGREGATION
+
+        $sql = "SELECT " 
+            . $table. ".id, " 
+            . $table.".name, GROUP_CONCAT(" . ($table === 'projektai' ? 'darbuotojai' : 'projektai' ) . ".name SEPARATOR \", \")" . " FROM " . $table . 
+        " LEFT JOIN " . ($table === 'projektai' ? 'darbuotojai' : 'projektai') . 
+        " ON " . ($table === 'projektai' ? 'darbuotojai.proj_id = projektai.id' : 'darbuotojai.proj_id = projektai.id') .
+        " GROUP BY " . $table . ".id;";
+
+        // TERNARY STATEMENTS
+        // if ($table === 'projektai') {return 'darbuotojai'} else (return 'projektai')
+        // (Condition) ? (Statement1) : (Statement2);
 
         $stmt = $conn->prepare($sql);
         $stmt->execute();
         $stmt->bind_result($id, $mainProjectName, $relatedEmployeeName);
 ?>
     <header>
-        <div>
+        <div class="header">
             <a href="?path=projektai" class="" style="">Projekto valdymas</a>
             <ul id="nav-mobile" class="left">
             <li><a href="?path=projektai">Projektai</a></li>
@@ -43,6 +58,7 @@
         </div>
     </header>
 
+    <main style="margin-left: 50px; margin-right: 50px">
     <?php
         echo '<table><th>Id</th><th>Name</th><th>' . ($table === 'projektai' ? 'Darbuotojai' : 'Projektai') . '</th>';
         while ($stmt->fetch()){
@@ -53,39 +69,30 @@
 
     <?php
 
-    // CREATE NEW EMPLOYEE LOGIC
+        // if(isset($_POST['ADD'])){
+        //     print($_POST['name']);
+        //     $sql_add = "INSERT INTO " . $table . " (`name`) VALUES (?)"; 
+        //     $stmt = $conn->prepare($sql_add);
+        //     $stmt -> bind_param("s", $_POST['name']);
+        //     $stmt->execute();
+        //     header("Location: /ProjectManagerPHP/?path=" . $_GET['path']);
+        // }
 
-        if(isset($_POST['create_employee'])){
-            $stmt = $conn->prepare('INSERT INTO darbuotojai (name) VALUES (?)');
-            $stmt->bind_param('s', $name);
-            $name = $_POST['name'];
-            
-            $stmt->execute();
-            $stmt->close();
-            header('Location: ' . $_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']);
-            die;
-        }
+    // $sql = 'SELECT * FROM darbuotojai';
+    // $result = $conn->query($sql);
 
-    $sql = 'SELECT * FROM darbuotojai';
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-          echo "id: " . $row["id"]. " - Name: " . $row["name"]. " ";
-          print("<button>DELETE</button><br>");
-        }
-    } else {
-        echo "0 results";
-    }
+    // if ($result->num_rows > 0) {
+    //     while($row = $result->fetch_assoc()) {
+    //       echo "id: " . $row["id"]. " - Name: " . $row["name"]. " ";
+    //       print("<button>DELETE</button><br>");
+    //     }
+    // } else {
+    //     echo "0 results";
+    // }
     
     $conn->close();
     ?>
 
-    <br>
-    <form action="" method="POST">
-        <label for="fname">Name:</label><br>
-        <input type="text" id="name" name="name" value=""><br>
-        <input type="submit" name="create_employee" value="Submit">
-    </form>
+</main>
 </body>
 </html>
